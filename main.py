9e35de5e7c795224b7e567282809cf41758ae1d4
xuedinge-zhang <2459277041@qq.com>
2026-07-18 16:35:03 +0800

    1

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("nihao")
 print("nihao")
 print("nihao")
 print("nihao")
+name = "传播博客"
+setup_year = 2006
+stock_price = 19.99
+message = ("%s成立于%d今天股价是%f")%(name,setup_year,stock_price)
+print(message)
+a = 111
